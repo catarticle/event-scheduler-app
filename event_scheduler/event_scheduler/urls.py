c44d4ca2a208efe2952660ff0a739659cccaffef
catarticle <arvsmirnova@gmail.com>
2025-04-06 22:39:29 +0300

    Fix urls, add test-frontend

--- event_scheduler/event_scheduler/urls.py
+++ event_scheduler/event_scheduler/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import urls as users_urls
+from home import urls as home_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include(users.urls)),
-    path('/', include(home.urls)),
+    path('auth/', include(users_urls)),
+    path('', include(home_urls)),
 ]
--- event_scheduler/users/urls.py
+++ event_scheduler/users/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.urls import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template="users/login.html"), name='login'),
+    path('login/', LoginView.as_view(template_name="users/login.html"), name='login'),
     path('logout/', LogoutView.as_view(next_page="/"), name='logout'),
-    path('registration/', registration_view, name='registration'),
+    path('registration/', views.registration_view, name='registration'),
 ]

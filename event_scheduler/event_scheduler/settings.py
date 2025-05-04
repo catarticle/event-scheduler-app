10dd1c23949b2062a9ae6dffb5d8a7af6ef3ad5d
catarticle <arvsmirnova@gmail.com>
2025-05-04 17:02:29 +0300

    Database connected

--- event_scheduler/event_scheduler/settings.py
+++ event_scheduler/event_scheduler/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'event_scheduler.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'event-scheduler-db',
+        'USER': 'postgres',
+        'PASSWORD': 'maIam2343PRO',  
+        'HOST': 'localhost',
+        'PORT': '5432', 
     }
 }
 
